099d0ec86b28750dc9ecefa53dfd57bf116b1677
William Janoti <wjanoti@gmail.com>
2019-02-13 02:40:36 +0100

    Exercise 1

--- oblig1/barrier.m
+++ oblig1/barrier.m
@@ -1,54 +1,60 @@
 const barrier <- monitor object barrierObj
 
-  % variable to keep track of how many processes are waiting.
+  % how many processes should be held at the barrier.
+  const maxProcesses : Integer <- 4
+
+  % variable to keep track of how many processes are currently waiting.
   var counter : Integer <- 0
 
   const c : Condition <- Condition.create
 
-  export operation enter
-    if counter < 4 then
-      counter <- counter + 1
-      stdout.putstring["One process entered the barrier. There are " || counter.asstring || " processes waiting\n"]
-      wait c
-    end if
-    % notify waiting processes
-    loop
-      exit when counter == 0
-      counter <- counter - 1
-      signal c
-      stdout.putstring["A process has been released.\n"]
-    end loop
+  export operation enter[objectName:String]
+    stdout.putstring["A process from object " || objectName || " has entered the barrier.\n"]
 
+    % notify waiting processes
+    if counter == maxProcesses then
+      loop
+        exit when counter == 0
+        counter <- counter - 1
+        signal c
+        stdout.putstring["A process has been released. \n"]
+      end loop
+    end if
+
+    counter <- counter + 1
+    stdout.putstring["There are " || counter.asstring || " processes waiting\n"]
+    wait c
   end enter
 
 end barrierObj
 
+% main program
 const main <- object main
-    const innerObject <- object innerObject
-      const p1 <- object p1
-        process
-          barrier.enter
-        end process
-      end p1
-      const p2 <- object p2
-        process
-          barrier.enter
-        end process
-      end p2
-      const p3 <- object p3
-        process
-          barrier.enter
-        end process
-      end p3
-      const p4 <- object p4
-        process
-          barrier.enter
-        end process
-      end p4
-      const p5 <- object p5
-        process
-          barrier.enter
-        end process
-      end p5
-    end innerObject
+  process
+    % process creation, 3 times, 4 processes
+    for i : Integer <- 0 while i < 3 by i <- i + 1
+      const innerObject <- object innerObject
+        const p1 <- object p1
+          process
+            barrier.enter[nameof self]
+          end process
+        end p1
+        const p2 <- object p2
+          process
+            barrier.enter[nameof self]
+          end process
+        end p2
+        const p3 <- object p3
+          process
+            barrier.enter[nameof self]
+          end process
+        end p3
+        const p4 <- object p4
+          process
+            barrier.enter[nameof self]
+          end process
+        end p4
+      end innerObject
+    end for
+  end process
 end main
