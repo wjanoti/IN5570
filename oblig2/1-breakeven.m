506f5a6bb0cd76e5ed81fbcf6bd99dc2bd1303be
William Janoti <wjanoti@gmail.com>
2019-03-04 02:17:24 +0100

    WIÅ

--- oblig2/1-breakeven.m
+++ oblig2/1-breakeven.m
@@ -1,41 +1,43 @@
-
 const main <- object main
+
+  const here <- locate self
+  const otherNodes <- here$activeNodes
+  const argObjType <- typeobject argObjType
+      operation getSize -> [Integer]
+  end argObjType
 
   % argument object
   const argObj <- object argObj
-    const arr <- Array.of[Integer].create[50000]
-    initially
-      (locate self)$stdout.putstring["Array variable is at: " || (locate arr)$name || "\n"]
-    end initially
+    attached const arr <- Array.of[Integer].create[50000]
+
+    export operation getSize -> [ size : Integer ]
+      size <- arr.upperbound + 1
+    end getSize
   end argObj
 
   % object that's gonna be moved to a different node.
   const remoteObj <- object remoteObj
-    export operation doSomething[arg: Any, origin: Node]
-      (locate self)$stdout.putstring["Arg comes from: " || (locate arg)$name || "\n"]
+    export operation doSomething[arg: argObjType, origin: Node]
+      (locate self)$stdout.PutString["The attached array in the parameter has : " || arg.getSize.asstring || " elements\n"]
       move arg to origin
     end doSomething
   end remoteObj
 
+  % gets alive nodes (assume that there's at least one) and moves the object that we are gonna invoke there
+  export op setUp
+    const otherNode <- otherNodes[1]$theNode
+    assert otherNode !== here
+    move remoteObj to otherNode
+    move argObj to otherNode
+  end setUp
+
   process
-    const here <- locate self
-    const all <- here$activeNodes
-    var there : Node
-
-    for i : Integer <- 0 while i <= all.upperbound by i <- i + 1
-      const aNode <- all[i]$theNode
-      if aNode !== here and aNode$name != here$name then
-       there <- all[i]$theNode
-      end if
-    end for
-
-    move remoteObj to there
-    move argObj to there
-
+    self.setUp
     const starttime <- here$timeOfDay
     remoteObj.doSomething[argObj, here]
     const endtime <- here$timeOfDay
-    stdout.putstring["Remote invokes took "||(endtime-starttime).asString||" seconds\n"]
-    stdout.putstring["remote obj is in " || (locate remoteObj)$name || "\n"]
+    const removeInvocationTime <- endtime - starttime
+    stdout.putstring["Remote invokation took: " || removeInvocationTime.asString ||" seconds\n"]
+    stdout.PutString["Break even = " || (removeInvocationTime / 32).asString || "\n"]
   end process
 end main
