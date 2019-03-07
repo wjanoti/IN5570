1c233635826fd33930ae8207145d283aee887e34
William Janoti <w.janoti@sportradar.com>
2019-03-07 21:49:21 +0100

    Exercise 1 - break even

--- oblig2/1-breakeven.m
+++ oblig2/1-breakeven.m
@@ -1,43 +1,66 @@
-const main <- object main
+% Argument object
+const Argument <- class Argument[ arraySize : Integer ]
+  attached var arr : Array.of[Integer]
 
+  % Simple operation to be called by the remote object.
+  export operation getArraySize -> [ n : Integer]
+    n <- arr.upperbound + 1
+  end getArraySize
+
+  initially
+    arr <- Array.of[Integer].create[arraySize]
+  end initially
+end Argument
+
+% Object that's gonna be moved to a different node.
+const remoteObj <- object remoteObj
+  % Operation that accesses the internal array in the argument object remotely.
+  export operation callRemote[arg: Argument]
+    (locate self)$stdout.PutString["The attached array in the parameter object has : " || arg.getArraySize.asstring || " elements\n"]
+  end callRemote
+
+  % Operation that accesses the internal array in the argument object by visit.
+  export operation callByVisit[arg: Argument, origin: Node]
+    (locate self)$stdout.PutString["The attached array in the parameter object has : " || arg.getArraySize.asstring || " elements\n"]
+    move arg to origin
+  end callByVisit
+end remoteObj
+
+const main <- object main
   const here <- locate self
   const otherNodes <- here$activeNodes
-  const argObjType <- typeobject argObjType
-      operation getSize -> [Integer]
-  end argObjType
-
-  % argument object
-  const argObj <- object argObj
-    attached const arr <- Array.of[Integer].create[50000]
-
-    export operation getSize -> [ size : Integer ]
-      size <- arr.upperbound + 1
-    end getSize
-  end argObj
-
-  % object that's gonna be moved to a different node.
-  const remoteObj <- object remoteObj
-    export operation doSomething[arg: argObjType, origin: Node]
-      (locate self)$stdout.PutString["The attached array in the parameter has : " || arg.getSize.asstring || " elements\n"]
-      move arg to origin
-    end doSomething
-  end remoteObj
+  const argObj <- Argument.create[10000]
+  var otherNode : Node
+  var starttime : Time
+  var endtime : Time
 
   % gets alive nodes (assume that there's at least one) and moves the object that we are gonna invoke there
   export op setUp
-    const otherNode <- otherNodes[1]$theNode
-    assert otherNode !== here
+    otherNode <- otherNodes[1]$theNode
     move remoteObj to otherNode
-    move argObj to otherNode
   end setUp
 
   process
     self.setUp
-    const starttime <- here$timeOfDay
-    remoteObj.doSomething[argObj, here]
-    const endtime <- here$timeOfDay
+
+    % call the remote object without moving the parameter.
+    starttime <- here$timeOfDay
+    remoteObj.callRemote[argObj]
+    endtime <- here$timeOfDay
     const removeInvocationTime <- endtime - starttime
-    stdout.putstring["Remote invokation took: " || removeInvocationTime.asString ||" seconds\n"]
-    stdout.PutString["Break even = " || (removeInvocationTime / 32).asString || "\n"]
+    const removeInvocationSeconds <- removeInvocationTime.getSeconds.asreal + (removeInvocationTime.getMicroSeconds.asreal/1000000.0)
+    stdout.putstring["Array size = " || argObj.getArraySize.asstring || "\n"]
+    stdout.putstring[" => Remote invocation: " || removeInvocationSeconds.asstring || " seconds\n"]
+
+    % call the remote object by visit
+    move argObj to otherNode
+    starttime <- here$timeOfDay
+    remoteObj.callByVisit[argObj, here]
+    endtime <- here$timeOfDay
+    const callByVisitInvocationTime <- endtime - starttime
+    const callByVisitInvocationSeconds <- (callByVisitInvocationTime.getSeconds.asreal + (callByVisitInvocationTime.getMicroSeconds.asreal/1000000.0))
+    stdout.putstring[" => Call by visit invocation: " || callByVisitInvocationSeconds.asstring || " seconds\n"]
+
+    stdout.putstring["Break-even: ~ " || ((callByVisitInvocationSeconds/removeInvocationSeconds) - 1.0).asString || " calls \n"]
   end process
 end main
