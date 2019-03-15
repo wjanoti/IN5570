be0d3ea5560e4f41adbed019a2f5422326e81d7a
William Janoti <wjanoti@gmail.com>
2019-03-15 04:24:23 +0100

    Oblig 2 - ex 3

--- oblig2/3-time_synchronizer.m
+++ oblig2/3-time_synchronizer.m
@@ -42,7 +42,7 @@ const TimeSynchronizer <- object TimeSynchronizer
     % fix agents at nodes
     for i : Integer <- 0 while i < allNodes.upperbound + 1 by i <- i + 1
       aNode <- allNodes[i]$theNode
-      agent <-TimeAgent.create[]
+      agent <- TimeAgent.create[]
       timeAgents.addUpper[agent]
       fix agent at aNode
     end for
@@ -53,7 +53,7 @@ const TimeSynchronizer <- object TimeSynchronizer
         timeSumMicroseconds <- 0.0
         nodeTimes <- Array.of[nodeTimeInfo].empty
         % query agents every minute
-        home.delay[Time.create[0, 15000000]]
+        home.delay[Time.create[0, 60000000]]
         home$stdout.putstring["\n"]
         for i : Integer <- 0 while i < timeAgents.upperbound + 1 by i <- i + 1
           agent <- timeAgents[i]
@@ -61,14 +61,20 @@ const TimeSynchronizer <- object TimeSynchronizer
           requestStartTime <- home$timeOfDay
           nodeTime <- agent.getLocalTime
           requestEndTime <- home$timeOfDay
-          roundTripTime <- requestStartTime - requestEndTime
-          timeSumSeconds <- timeSumSeconds + nodeTime.getSeconds.asreal
-          timeSumMicroseconds <- timeSumMicroseconds + nodeTime.getMicroSeconds.asreal
+          % used to take into account the response time from the agent, assuming that request and response take about the same time.
+          roundTripTime <- requestEndTime - requestStartTime
+          timeSumSeconds <- timeSumSeconds + nodeTime.getSeconds.asreal + (roundTripTime.getSeconds.asreal/2.0)
+          timeSumMicroseconds <- timeSumMicroseconds + nodeTime.getMicroSeconds.asreal + (roundTripTime.getMicroSeconds.asreal/2.0)
           nodeTimes.addUpper[nodeTimeInfo.create[nodeTime, agent.getNodeName]]
         end for
 
-        synchTime <- Time.create[(timeSumSeconds/(nodeTimes.upperbound + 1).asreal).asinteger, (timeSumMicroseconds/(nodeTimes.upperbound + 1).asreal).asinteger]
-        home$stdout.putstring["Time aver is : " || synchTime.asDate || "\n"]
+        const numberOfTimeSamples <- nodeTimes.upperbound + 1
+        % average of collected times
+        synchTime <- Time.create[(timeSumSeconds/numberOfTimeSamples.asreal).asinteger, (timeSumMicroseconds/numberOfTimeSamples.asreal).asinteger]
+        home$stdout.putstring["Approximate current time: " || synchTime.asdate || " (" || synchTime.asString || ")\n"]
+        for i : Integer <- 0 while i < nodeTimes.upperbound + 1 by i <- i + 1
+          home$stdout.putstring["\nNode " || nodeTimes[i]$nodeName || "\n - Difference to approximated time:  (" || nodeTimes[i]$nodeTime.asstring || ") = " || (synchTime - nodeTimes[i]$nodeTime).asstring || "\n"]
+        end for
 
         unavailable
           (locate self)$stdout.putstring["Couldn't get time, node unavailable\n"]
