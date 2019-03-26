49ccc3911a291d8309310ddd23c8962267f61155
William Janoti <wjanoti@gmail.com>
2019-03-26 04:21:07 +0100

    work work

--- he1/nopester.m
+++ he1/nopester.m
@@ -31,7 +31,8 @@ end djb2
 const NopesterServer <- object NopesterServer
   const home <- locate self
   var peerList : Array.of[PeerType] <- Array.of[PeerType].empty
-  var filesIndex : Directory <- Directory.create
+  var filesPeerIndex : Directory <- Directory.create
+  var fileNamesIndex : Directory <- Directory.create
 
   export operation addPeer[ peer : PeerType ]
     peerList.addUpper[peer]
@@ -39,32 +40,29 @@ const NopesterServer <- object NopesterServer
 
   export operation registerFile[ fileName: String, fileHash : Integer, peer : PeerType ]
     var filePeers : Array.of[PeerType]
-    if filesIndex.lookup[fileHash.asString] == nil then
+    if filesPeerIndex.lookup[fileHash.asString] == nil then
       filePeers <- Array.of[PeerType].empty
     else
-      filePeers <- view filesIndex.lookup[fileHash.asString] as Array.of[PeerType]
+      filePeers <- view filesPeerIndex.lookup[fileHash.asString] as Array.of[PeerType]
     end if
     filePeers.addUpper[peer]
-    filesIndex.insert[fileHash.asString, filePeers]
+    filesPeerIndex.insert[fileHash.asString, filePeers]
+    fileNamesIndex.insert[fileHash.asString, fileName]
   end registerFile
 
   export operation listAvailableFiles
-    var peerFileList : Array.of[FileRecord]
-    stdout.putstring["Available files\n"]
-    for i : Integer <- 0 while i <= peerList.upperbound by i <- i + 1
-      if peerList[i] !== nil then
-        peerFileList  <- peerList[i].getFiles
-        for j : Integer <- 0 while j <= peerFileList.upperbound by j <- j + 1
-          stdout.PutString["-" || peerFileList[j].getfileName || "@" || (locate peerList[i])$name || "\n"]
-        end for
-      end if
+    const files <- fileNamesIndex.list
+    stdout.putstring["Available files: \n"]
+    for i : Integer <- 0 while i <= files.upperbound by i <- i + 1
+        var fileName : String <- view fileNamesIndex.lookup[files[i]] as String
+        stdout.putstring["  -> " || fileName || "\n"]
     end for
   end listAvailableFiles
 
   export operation listFiles
-    const files <- filesIndex.list
+    const files <- filesPeerIndex.list
     for i : Integer <- 0 while i <= files.upperbound by i <- i + 1
-        const pList <- view filesIndex.lookup[files[i]] as Array.of[PeerType]
+        const pList <- view filesPeerIndex.lookup[files[i]] as Array.of[PeerType]
         var nodesNames : String <- ""
         for j : Integer <- 0 while j <= pList.upperbound by j <- j + 1
             nodesNames <- nodesNames || (locate pList[j])$name || " - "
@@ -97,8 +95,7 @@ const NopesterServer <- object NopesterServer
 end NopesterServer
 
 
-const Peer <- class PeerClass [ server : Node ]
-    const serverNode <- server
+const Peer <- class PeerClass
     const hashImplementation <- djb2
     attached const files <- Array.of[FileRecord].empty
 
@@ -128,13 +125,13 @@ const test <- object test
     for i : Integer <- 0 while i < activeNodes.upperbound + 1 by i <- i + 1
       aNode <- activeNodes[i].getTheNode
       if aNode !== home then
-        peerObject <- Peer.create[home]
-        peerObject.addFile["a.mp3"]
+        peerObject <- Peer.create
+        peerObject.addFile[testFiles[i]]
         fix peerObject at aNode
         NopesterServer.addPeer[peerObject]
       end if
     end for
-    NopesterServer.listFiles
-  %  NopesterServer.listAvailableFiles
+  %  NopesterServer.listFiles
+    NopesterServer.listAvailableFiles
   end initially
 end test
