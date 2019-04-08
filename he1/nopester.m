1d61331b84f1910f13b2704a688ddc3663ce203e
William Janoti <wjanoti@gmail.com>
2019-04-08 03:58:29 +0200

    Building test

--- he1/nopester.m
+++ he1/nopester.m
@@ -3,6 +3,7 @@ const nopester <- object nopester
   var aNode : Node
   var aPeer : PeerType
   var peerObject : PeerType
+  var p1, p2, p3, p4, p5 : PeerType
   const activeNodes <- (locate self).getActiveNodes
   const peers <- Array.of[PeerType].empty
   const testFiles  <- {
@@ -18,35 +19,56 @@ const nopester <- object nopester
     "Pack my box with five dozen liquor jugs"
   }
 
+  % creates a peer and add files on it.
   operation createPeer [ name : String, files : ImmutableVector.of[String] ] -> [ newPeer : PeerType ]
       peerObject <- Peer.create[name, Server]
       for i : Integer <- 0 while i <= files.upperbound by i <- i + 1
+        % file names will have the following convention: peer name + "f" + file number, e.g. p1f1
         peerObject.addFile[name || "f" || i.asstring, files[i]]
       end for
       newPeer <- peerObject
   end createPeer
 
-  initially
+  % creates 5 peers and adds a set of files on each of them
+  operation setup
     fix Server at here
-    const p1 <- self.createPeer["p1", testFiles.getSlice[0, 3]]
+    p1 <- self.createPeer["p1", testFiles.getSlice[0, 3]]
     peers.addUpper[p1]
-    const p2 <- self.createPeer["p2", testFiles.getSlice[2, 3]]
+    p2 <- self.createPeer["p2", testFiles.getSlice[2, 3]]
     peers.addUpper[p2]
-    const p3 <- self.createPeer["p3", testFiles.getSlice[4, 3]]
+    p3 <- self.createPeer["p3", testFiles.getSlice[4, 3]]
     peers.addUpper[p3]
-    const p4 <- self.createPeer["p4", testFiles.getSlice[6, 3]]
+    p4 <- self.createPeer["p4", testFiles.getSlice[6, 3]]
     peers.addUpper[p4]
-    const p5 <- self.createPeer["p5", testFiles.getSlice[7, 3]]
+    p5 <- self.createPeer["p5", testFiles.getSlice[7, 3]]
     peers.addUpper[p5]
 
-    %for i : Integer <- 0 while i <= activeNodes.upperbound by i <- i + 1
-    %  aNode <- activeNodes[i].getTheNode
-    %  if aNode !== here then
-    %    aPeer <- peers.removeUpper
-    %    fix aPeer at aNode
-    %    Server.addPeer[aPeer]
-    %  end if
-    %end for
+    % distribute the peers among the nodes
+    for i : Integer <- 0 while i <= activeNodes.upperbound by i <- i + 1
+      aNode <- activeNodes[i].getTheNode
+      % we consider the node we run this program the server and don't want any peer objects here.
+      if aNode !== here then
+        if i == 1 then
+          loop
+            exit when peers.upperbound == 2
+            aPeer <- peers.removeUpper
+            fix aPeer at aNode
+            Server.addPeer[aPeer]
+          end loop
+        else
+          loop
+            exit when peers.upperbound == -1
+            aPeer <- peers.removeUpper
+            fix aPeer at aNode
+            Server.addPeer[aPeer]
+          end loop
+        end if
+      end if
+    end for
+  end setup
+
+  initially
+    self.setup
     %const possiblePeers <- p2.searchFileByName["p1f1"]
     %for i : Integer <- 0 while i <= possiblePeers.upperbound by i <- i + 1
     %  begin
