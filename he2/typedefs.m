46e58a8ee500f3669b5d62ae6e60f02abe365a42
William Janoti <wjanoti@gmail.com>
2019-04-18 16:25:55 +0200

    WIP

--- he2/typedefs.m
+++ he2/typedefs.m
@@ -1,15 +1,16 @@
 const PCRType <- typeobject PCRType
   operation replicate[X : t, N : Integer]
-  forall t
-  suchthat
-    t *> typeobject ot
-      op clone -> [result : t]
-    end ot
-  operation replicas[X : t] -> [Array.of[rt]]
-  forall t
-  where
-    rt <- typeobject rt
-      operation read -> [o : t]
-      operation write[o : t]
-    end rt
+    forall t
+    suchthat
+      t *> typeobject objectType
+              op clone -> [result : t]
+           end objectType
+
+  operation replicas[X : t] -> [Array.of[ReplicaType]]
+    forall t
+    where
+      ReplicaType <- typeobject replicaType
+                        operation read -> [o : t]
+                        operation write[o : t]
+                    end replicaType
 end PCRType
